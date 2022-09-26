19adf978d8f1581243c4773a86ea0e3dc73df62c
Aseelyaser <aseelyaserco@gmail.com>
2022-09-26 21:49:44 +0300

    get all cart

--- starter-code/backend/controllers/cart.js
+++ starter-code/backend/controllers/cart.js
@@ -2,22 +2,21 @@ const cartModel = require("../models/cartSchema");
 
 //------------- create new cart -------------
 const createNewCart = (req, res) => {
-  const { user, book ,quantity } = req.body;//q
+  const { user, book, quantity } = req.body; //q
   const cartModelInstance = new cartModel({
     user,
     book,
-    quantity
+    quantity,
   });
-  cartModelInstance
-  .save()
-  .then((result)=>{
+  cartModelInstance.save().then((result) => {
     res.status(201);
-    res.json({
-        success:true,
+    res
+      .json({
+        success: true,
         message: "cart created",
         cart: result,
-    })
-    .catch((err) => {
+      })
+      .catch((err) => {
         res.status(500);
         res.json({
           success: false,
@@ -27,16 +26,33 @@ const createNewCart = (req, res) => {
       });
   });
 };
-
-
+//------------- create all cart -------------
+const getAllCart = (req, res) => {
+  cartModel
+    .find()
+    .populate("book", "price -_id").select(" -_id")
+    .exec()
+    .then((result) => {
+      res.status(200);
+      res.json({
+        success: true,
+        message: "All the cart",
+        cart: result,
+      });
+    })
+    .catch((err) => {
+      res.staus(500);
+      res.json({
+        success: false,
+        message: "Server Error",
+        err:err.message,
+      });
+    });
+};
 //------------------------------------------
-module.exports = {createNewCart};
-
-
+module.exports = { createNewCart,getAllCart };
 
 //CRUD   ..> CART
 // iduser
 // idbook
 // total
-// .populate("book", "price -_id")
-// .exec()
--- starter-code/backend/routes/cart.js
+++ starter-code/backend/routes/cart.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { createNewCart } = require('../controllers/cart');
+const { createNewCart,getAllCart } = require('../controllers/cart');
 
 const authentication =require('../middlewares/authentication');
 const authorization =require('../middlewares/authorization');
 
 const cartRouter = express.Router();
 cartRouter.post("/",authentication,authorization("CREATE_CART"),createNewCart);
+cartRouter.get("/",authentication,getAllCart);
 
 module.exports = cartRouter;
